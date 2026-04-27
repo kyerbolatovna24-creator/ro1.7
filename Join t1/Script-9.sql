6843a94383d541b2e34b259c0c5f06883268f766
kyerbolatovna24-creator <k.yerbolatovna24@apec.edu.kz>
2026-04-27 18:45:02 +0500

    Add files via upload

---
+++ Join t1/Script-9.sql
@@ -0,0 +1,8 @@
+SELECT 
+    f.title,
+    c.name AS category_name
+FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+ORDER BY f.title ASC
+LIMIT 20;
